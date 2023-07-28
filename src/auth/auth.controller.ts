65706f7c678c7337851a888aab858773988c0926
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-07-29 00:16:25 +0330

    create dto for receive data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { Request } from "express";
+import { AuthDto } from "./dto";
 
 @Controller("auth")
 export class AuthController {
     constructor(private authService: AuthService){}
 
     @Post('signup')
-    signup(@Body() dto:any) {
+    signup(@Body() dto:AuthDto) {
         console.log({dto,})
         return this.authService.signup();
     }
