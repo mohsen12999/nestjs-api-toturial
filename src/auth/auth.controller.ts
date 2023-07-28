e04760ace34a85c142fdf43ffc9e836b6937394d
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-07-28 09:49:53 +0330

    add new route in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller()
 export class AuthController {
     constructor(private authService: AuthService){}
+
+    @Post('signup')
+    signup() {
+        return "I am signed up"
+    }
+
+    @Post("signin")
+    signin() {
+        return "I am signed in"
+    }
 }
