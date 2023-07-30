6de198d7ef50ed571f27b392fc2e7824bc0eeca4
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-07-30 09:58:07 +0330

    add user in database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma:PrismaService ){}
 
-    signup() {
-        return { "msg": "I am signed up" };
+    async signup(dto:AuthDto) {
+        // generate the new user in the db
+        const hash = await argon.hash(dto.password);
+
+        // save the new user in the db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            }
+        })
+        
+        // return the saved user
+        return user;
     }
 
     signin() {
