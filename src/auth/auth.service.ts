56b16f8df6df5f30c7074c7d454744ac86b53549
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-07-30 10:26:48 +0330

    creat sign in method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -36,7 +36,29 @@ export class AuthService {
 
     }
 
-    signin() {
-        return { "msg": "I am signed in" };
+    async signin(dto: AuthDto) {
+
+        // find the user by email
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email: dto.email,
+            }
+        });
+
+        // if user does not exist throw exception
+        if (!user) {
+            throw new ForbiddenException('Credentials incorrect');
+        }
+
+        // compare password
+        const pwMatches = await argon.verify(user.hash, dto.password);
+
+        // if password incorrect throw exception
+        if(!pwMatches) {
+            throw new ForbiddenException('Credentials incorrect');
+        }
+
+        // send back user
+        return user;
     }
 }
