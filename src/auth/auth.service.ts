5c6510d96963179b2b28484f9f6015d0dc3be9fe
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-07-30 10:15:00 +0330

    error handling for repeated email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma:PrismaService ){}
+    constructor(private prisma: PrismaService) { }
 
-    async signup(dto:AuthDto) {
+    async signup(dto: AuthDto) {
         // generate the new user in the db
         const hash = await argon.hash(dto.password);
 
-        // save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
+        try {
+
+            // save the new user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            })
+
+            // return the saved user
+            return user;
+
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials token')
+                }
             }
-        })
-        
-        // return the saved user
-        return user;
+            throw error;
+        }
+
     }
 
     signin() {
